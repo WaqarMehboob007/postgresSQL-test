14b466f2365c78dac7d1577bb634beecc3ef0d4e
waqarmehboob <cgs.waqar@gmail.com>
2023-12-03 08:04:09 +0500

    Vercel Postgres SQL Practice

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from 'next/image'
+
+export default function Home() {
+  console.log("this is client component")
+  return (
+    <div>
+      Hello SQL
+
+    </div>
+  )
+}
